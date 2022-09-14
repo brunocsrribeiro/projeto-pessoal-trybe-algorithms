2bd4835b9de61e6f3b61e632fdc56cd0ad0a65ca
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-09-14 18:45:20 -0300

    Feat: Requirement 1 complete.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if not target_time:
+        return None
+
+    schedule = 0
+    for prohibited, exit in permanence_period:
+        if type(prohibited) != int or type(exit) != int:
+            return None
+        elif prohibited <= target_time <= exit:
+            schedule += 1
+
+    return schedule
