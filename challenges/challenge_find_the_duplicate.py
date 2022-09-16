c48216e1e385bf696035a4ebaeec167f6a1dca5a
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-09-16 19:29:59 -0300

    Feat: Requirement 4 complete

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,7 +1,19 @@
 def find_duplicate(nums):
-    if not nums or len(nums) < 2 or nums != list:
+    if not nums or len(nums) < 2 or type(nums) != list:
         return False
 
-    for idx in nums:
-        if nums[idx] != int:
+    for i in nums:
+        if type(i) != int or i < 0:
             return False
+
+    nums.sort()
+    index = 0
+    repeated_number = None
+
+    while index < (len(nums) - 1):
+        if nums[index] == nums[index + 1]:
+            repeated_number = nums[index]
+        index += 1
+    if repeated_number is None:
+        return False
+    return repeated_number
